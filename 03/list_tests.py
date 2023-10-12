e11ddaeeff913fec06a89ac0c0ccba07c516b846
Darya2477 <85761375+Darya2477@users.noreply.github.com>
2023-10-12 16:46:54 +0300

    Update list_tests.py

--- 03/list_tests.py
+++ 03/list_tests.py
@@ -1,7 +1,7 @@
 from list import CustomList
 import unittest
 
-class TestList(unittest.TestCase):
+class TestCustomList(unittest.TestCase):
 
     def setUp(self):
        pass
@@ -134,10 +134,373 @@ class TestList(unittest.TestCase):
 
     #     cl_1.add(cl_2)
 
-    def test(self):
+    def test_add_equal_lengths_cl2_is_list(self):
         cl_1 = CustomList([1,2,3,4,5])
+        cl_2  = [1,2,3,4,5]
+
+        self.assertListEqual( cl_1.__add__(cl_2), [2,4,6,8,10])
+
+
+    def test_add_non_equal_lengths_cl2_is_list(self):
+        cl_1 = CustomList([1,2,3,4,5])
+        cl_2  = [1,2]
+
+        self.assertListEqual(cl_1.__add__(cl_2), [2,4,3,4,5])
+
+    def test_add_cl1_length_is_zero_cl2_is_list(self):
+        cl_1 = CustomList([])
+        cl_2  = [1,2]
+
+        self.assertListEqual(cl_1.__add__(cl_2), [1,2])
+
+    def test_add_cl2_length_is_zero_cl2_is_list(self):
+        cl_1 = CustomList([1,2])
+        cl_2  = []
+
+        self.assertListEqual(cl_1.__add__(cl_2), [1,2])
+
+    def test_add_all_lengths_are_zero_cl2_is_list(self):
+        cl_1 = CustomList([])
+        cl_2  = []
+
+        self.assertListEqual(cl_1.__add__(cl_2), [])
+
+    def test_add_new_list_is_the_new_obj_cl2_is_list(self):
+        cl_1 = CustomList([1,2,3,4,5])
+        cl1_id = id(cl_1)
+        cl_2  = [1,2,3]
+        cl2_id = id(cl_2)
+
+        res = cl_1.__add__(cl_2)
+
+        self.assertNotEqual(id(res), cl1_id)
+        self.assertNotEqual(id(res), cl2_id)
+
+    def test_add_old_lists_are_the_old_obj_cl2_is_list(self):
+        cl_1 = CustomList([1,2,3,4,5])
+        cl1_id = id(cl_1)
+        cl_2  = [1,2,3]
+        cl2_id = id(cl_2)
+
+        cl_1.__add__(cl_2)
+
+        self.assertEqual(cl1_id, id(cl_1))
+        self.assertEqual(cl2_id, id(cl_2))
+
+
+    # def test_add_equal_lengths_cl1_is_list(self):
+    #     cl_1 = [1,2,3,4,5]
+    #     cl_2  = CustomList([1,2,3,4,5])
+
+    #     self.assertListEqual( cl_1.__add__(cl_2), [2,4,6,8,10])
+
+
+    # def test_add_non_equal_lengths_cl1_is_list(self):
+    #     cl_1 = [1,2,3,4,5]
+    #     cl_2  = CustomList([1,2])
+
+    #     self.assertListEqual(cl_1.__add__(cl_2), [2,4,3,4,5])
+
+    # def test_add_cl1_length_is_zero_cl1_is_list(self):
+    #     cl_1 = []
+    #     cl_2  = CustomList([1,2])
+
+    #     self.assertListEqual(cl_1.__add__(cl_2), [1,2])
+
+    # def test_add_cl2_length_is_zero_cl1_is_list(self):
+    #     cl_1 = [1,2]
+    #     cl_2  = CustomList([])
+
+    #     self.assertListEqual(cl_1.__add__(cl_2), [1,2])
+
+    # def test_add_all_lengths_are_zero_cl1_is_list(self):
+    #     cl_1 = []
+    #     cl_2  = CustomList([])
+
+    #     self.assertListEqual(cl_1.__add__(cl_2), [])
+
+    # def test_add_new_list_is_the_new_obj_cl1_is_list(self):
+    #     cl_1 = [1,2,3,4,5]
+    #     cl1_id = id(cl_1)
+    #     cl_2  = CustomList([1,2,3])
+    #     cl2_id = id(cl_2)
+
+    #     res = cl_1.__add__(cl_2)
+
+    #     self.assertNotEqual(id(res), cl1_id)
+    #     self.assertNotEqual(id(res), cl2_id)
+
+    # def test_add_old_lists_are_the_old_obj_cl1_is_list(self):
+    #     cl_1 = [1,2,3,4,5]
+    #     cl1_id = id(cl_1)
+    #     cl_2  = CustomList([1,2,3])
+    #     cl2_id = id(cl_2)
+
+    #     cl_1.__add__(cl_2)
+
+    #     self.assertEqual(cl1_id, id(cl_1))
+    #     self.assertEqual(cl2_id, id(cl_2))
+
+
+
+################################################## SUB ######################################
+
+    
+    def test_sub_equal_lengths(self):
+        cl_1 = CustomList([1,2,3,4,5])
+        cl_2  = CustomList([1,2,3,4,5])
+
+        self.assertListEqual( cl_1.__sub__(cl_2), [0,0,0,0,0])
+
+    def test_sub_non_equal_lengths(self):
+        cl_1 = CustomList([1,2,3,4,5])
+        cl_2  = CustomList([1,2])
+
+        self.assertListEqual(cl_1.__sub__(cl_2), [0,0,3,4,5])
+
+    def test_sub_cl1_length_is_zero(self):
+        cl_1 = CustomList([])
+        cl_2  = CustomList([1,2])
+
+        self.assertListEqual(cl_1.__sub__(cl_2), [-1,-2])
+
+    def test_sub_cl2_length_is_zero(self):
+        cl_1 = CustomList([1,2])
+        cl_2  = CustomList([])
+
+        self.assertListEqual(cl_1.__sub__(cl_2), [1,2])
+
+    def test_sub_all_lengths_are_zero(self):
+        cl_1 = CustomList([])
+        cl_2  = CustomList([])
+
+        self.assertListEqual(cl_1.__sub__(cl_2), [])
+
+    def test_sub_new_list_is_the_new_obj(self):
+        cl_1 = CustomList([1,2,3,4,5])
+        cl1_id = id(cl_1)
         cl_2  = CustomList([1,2,3])
-        print(cl_1+cl_2)
+        cl2_id = id(cl_2)
+
+        res = cl_1.__sub__(cl_2)
+
+        self.assertNotEqual(id(res), cl1_id)
+        self.assertNotEqual(id(res), cl2_id)
+
+    def test_sub_old_lists_are_the_old_obj(self):
+        cl_1 = CustomList([1,2,3,4,5])
+        cl1_id = id(cl_1)
+        cl_2  = CustomList([1,2,3])
+        cl2_id = id(cl_2)
+
+        cl_1.__sub__(cl_2)
+
+        self.assertEqual(cl1_id, id(cl_1))
+        self.assertEqual(cl2_id, id(cl_2))
+
+    
+
+
+    # ТЕСТЫ НА НУЛИ В СПИСКАХ,ГДЕ МЕНЬШЕ ПЕРАМЕТРОВ
+    # def test_add_cl1_less_length(self):
+    #     cl_1 = CustomList([1,2])
+    #     cl_2 = CustomList([1,2,3,4,5])
+
+    #     cl_1.add(cl_2)
+
+
+
+
+    def test_sub_equal_lengths_cl2_is_list(self):
+        cl_1 = CustomList([1,2,3,4,5])
+        cl_2  = [1,2,3,4,5]
+
+        self.assertListEqual( cl_1.__sub__(cl_2), [0,0,0,0,0])
+
+
+    def test_sub_non_equal_lengths_cl2_is_list(self):
+        cl_1 = CustomList([1,2,3,4,5])
+        cl_2  = [1,2]
+
+        self.assertListEqual(cl_1.__sub__(cl_2), [0,0,3,4,5])
+
+    def test_sub_cl1_length_is_zero_cl2_is_list(self):
+        cl_1 = CustomList([])
+        cl_2  = [1,2]
+
+        self.assertListEqual(cl_1.__sub__(cl_2), [-1,-2])
+
+    def test_sub_cl2_length_is_zero_cl2_is_list(self):
+        cl_1 = CustomList([1,2])
+        cl_2  = []
+
+        self.assertListEqual(cl_1.__sub__(cl_2), [1,2])
+
+    def test_sub_all_lengths_are_zero_cl2_is_list(self):
+        cl_1 = CustomList([])
+        cl_2  = []
+
+        self.assertListEqual(cl_1.__sub__(cl_2), [])
+
+    def test_sub_new_list_is_the_new_obj_cl2_is_list(self):
+        cl_1 = CustomList([1,2,3,4,5])
+        cl1_id = id(cl_1)
+        cl_2  = [1,2,3]
+        cl2_id = id(cl_2)
+
+        res = cl_1.__sub__(cl_2)
+
+        self.assertNotEqual(id(res), cl1_id)
+        self.assertNotEqual(id(res), cl2_id)
+
+    def test_sub_old_lists_are_the_old_obj_cl2_is_list(self):
+        cl_1 = CustomList([1,2,3,4,5])
+        cl1_id = id(cl_1)
+        cl_2  = [1,2,3]
+        cl2_id = id(cl_2)
+
+        cl_1.__sub__(cl_2)
+
+        self.assertEqual(cl1_id, id(cl_1))
+        self.assertEqual(cl2_id, id(cl_2))
+
+
+ # def test_add_equal_lengths_cl1_is_list(self):
+    #     cl_1 = [1,2,3,4,5]
+    #     cl_2  = CustomList([1,2,3,4,5])
+
+    #     self.assertListEqual( cl_1.__add__(cl_2), [2,4,6,8,10])
+
+
+    # def test_add_non_equal_lengths_cl1_is_list(self):
+    #     cl_1 = [1,2,3,4,5]
+    #     cl_2  = CustomList([1,2])
+
+    #     self.assertListEqual(cl_1.__add__(cl_2), [2,4,3,4,5])
+
+    # def test_add_cl1_length_is_zero_cl1_is_list(self):
+    #     cl_1 = []
+    #     cl_2  = CustomList([1,2])
+
+    #     self.assertListEqual(cl_1.__add__(cl_2), [1,2])
+
+    # def test_add_cl2_length_is_zero_cl1_is_list(self):
+    #     cl_1 = [1,2]
+    #     cl_2  = CustomList([])
+
+    #     self.assertListEqual(cl_1.__add__(cl_2), [1,2])
+
+    # def test_add_all_lengths_are_zero_cl1_is_list(self):
+    #     cl_1 = []
+    #     cl_2  = CustomList([])
+
+    #     self.assertListEqual(cl_1.__add__(cl_2), [])
+
+    # def test_add_new_list_is_the_new_obj_cl1_is_list(self):
+    #     cl_1 = [1,2,3,4,5]
+    #     cl1_id = id(cl_1)
+    #     cl_2  = CustomList([1,2,3])
+    #     cl2_id = id(cl_2)
+
+    #     res = cl_1.__add__(cl_2)
+
+    #     self.assertNotEqual(id(res), cl1_id)
+    #     self.assertNotEqual(id(res), cl2_id)
+
+    # def test_add_old_lists_are_the_old_obj_cl1_is_list(self):
+    #     cl_1 = [1,2,3,4,5]
+    #     cl1_id = id(cl_1)
+    #     cl_2  = CustomList([1,2,3])
+    #     cl2_id = id(cl_2)
+
+    #     cl_1.__add__(cl_2)
+
+    #     self.assertEqual(cl1_id, id(cl_1))
+    #     self.assertEqual(cl2_id, id(cl_2))
+
+
+
+################################################ STR ##############################################
+
+
+    def test_str_empty_list(self):
+        self.assertEqual(([], 0), CustomList([]).__str__())
+
+    def test_str_not_empty_list(self):
+        self.assertEqual(([1,2,3,4,5], 15), CustomList([1,2,3,4,5]).__str__())
+
+    def test_one_elem_in_list(self):
+        self.assertEqual(([5],5), CustomList(CustomList([5])).__str__())
+ 
+
+
+
+################################################# EQ ############################################
+
+def test_eq_equal_lists(self):
+    cl_1 = CustomList([1,2,3,4,5])
+    cl_2 = CustomList([3,5,7])
+    self.assertEqual(True, cl_1.__eq__(cl_2))
+    self.assertEqual(True, cl_2.__eq__(cl_1))
+
+
+def test_eq_not_equal_lists(self):
+    cl_1 = CustomList([1,2,3,4,5])
+    cl_2 = CustomList([3,5,8])
+    self.assertEqual(False, cl_1.__eq__(cl_2))
+    self.assertEqual(False, cl_2.__eq__(cl_1))
+
+############################################### NE ################################################
+
+def test_ne_equal_lists(self):
+    cl_1 = CustomList([1,2,3,4,5])
+    cl_2 = CustomList([3,5,7])
+    self.assertEqual(False, cl_1.__ne__(cl_2))
+    self.assertEqual(False, cl_2.__ne__(cl_1))
+
+
+def test_ne_not_equal_lists(self):
+    cl_1 = CustomList([1,2,3,4,5])
+    cl_2 = CustomList([3,5,8])
+    self.assertEqual(True, cl_1.__ne__(cl_2))
+    self.assertEqual(True, cl_2.__ne__(cl_1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+   
+        
+
+
+
+
+
+
+       
 
 
 
