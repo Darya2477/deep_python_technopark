1798b9ec1ac009e70d251b30e6e07e5cd8016b4f
Darya2477 <85761375+Darya2477@users.noreply.github.com>
2023-10-12 17:48:18 +0300

    Update list.py

--- 03/list.py
+++ 03/list.py
@@ -1,4 +1,20 @@
+import copy
 class CustomList(list):
+
+    @staticmethod
+    def equate_list_length(first: 'CustomList', second: 'CustomList'):
+        first = copy.copy(first)
+        second = copy.copy(second)
+        max_len = max(len(first), len(second))
+
+        while (len(first)<max_len):
+            first.append(0)
+
+        while (len(second)<max_len):
+            second.append(0)
+        
+        return zip(first, second)
+    
     def __init__(self, custom_list: 'CustomList' ):
         super().__init__(custom_list)
 
@@ -14,62 +30,38 @@ class CustomList(list):
     @custom_list.deleter
     def custom_list(self):
         del self
-
-    def __add__(self, other: 'CustomList'):
-        max_len = max(len(self), len(other))
-
-        l1 = self
-        l2 = other
-        res=[]
-
-        while (len(l1)<max_len):
-            l1.append(0)
-
-        while (len(l2)<max_len):
-            l2.append(0)
         
-        for i in range(max_len):
-            res.append(l1[i]+l2[i])
-            
-        return res
-    
+    def __add__(self, other: 'CustomList'):
+        return ([x+y for x, y in CustomList.equate_list_length(self, other)])  
+      
     def __radd__(self, other:'CustomList'):
         if (isinstance(other, list)):
             return [x + y for x, y in zip(self, other)]
 
     def __sub__(self, other: 'CustomList'):
-        max_len = max(len(self), len(other))
-
-        l1 = self
-        l2 = other
-        res=[]
-
-        while (len(l1)<max_len):
-            l1.append(0)
-
-        while (len(l2)<max_len):
-            l2.append(0)
-
-        for i in range(max_len):
-            res.append(l1[i]-l2[i])
-            
-        return res
+        return [x-y for x, y in CustomList.equate_list_length(self, other)]
     
-
     def __eq__(self, other: 'CustomList'):
         return True if sum(self)==sum(other) else False
     
     def __ne__(self, other:'CustomList'):
         return True if sum(self)!=sum(other) else False
-        
-
+    
+    def __gt__(self, other: 'CustomList'):
+        return True if (sum(self))>sum(other) else False
+    
+    def __ge__(self, other: 'CustomList'):
+        return True if sum(self)>=sum(other) else False
+    
+    def __lt__(self, other:'CustomList'):
+        return True if sum(self)<sum(other) else False
+    
+    def __le__(self, other:'CustomList'):
+        return True if sum(self)<=sum(other) else False
         
     def __str__(self):
-        return (([x for x in self]), (sum(self)))
+        return (self, (sum(self)))
 
-
-
-        
 
 
         
