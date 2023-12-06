b7386212ef184a22c07110198ca9d18b3c39d105
Darya2477 <85761375+Darya2477@users.noreply.github.com>
2023-12-06 21:09:56 +0300

    Update lru.py

--- 05/lru.py
+++ 05/lru.py
@@ -1,5 +1,4 @@
 class LRUCache:
-
     class Node:
         def __init__(self, key, value):
             self.key = key
@@ -8,6 +7,8 @@ class LRUCache:
             self.next = None
 
     def __init__(self, limit=42):
+        if limit < 1:
+            raise Exception("Limit must be greater than or equal to 1.")
         self.limit = limit
         self.cache = {}
         self.head = self.Node(None, None)
@@ -18,29 +19,26 @@ class LRUCache:
     def get(self, key):
         if key in self.cache:
             node = self.cache[key]
-            self._move_to_front(node)  
+            self._move_to_front(node)
             return node.value
         return None
 
     def set(self, key, value):
-        if (self.limit>=1):
-            if key in self.cache:
-                node = self.cache[key]
-                node.value = value
-                self._move_to_front(node) 
-            else:
-                if len(self.cache) >= self.limit:
-                    self._evict_last() 
-                new_node = self.Node(key, value)
-                self._add_to_front(new_node) 
-                self.cache[key] = new_node
+        if key in self.cache:
+            node = self.cache[key]
+            node.value = value
+            self._move_to_front(node)
         else:
-            raise Exception('Limit must be greater than or equal to 1.')
+            if len(self.cache) >= self.limit:
+                self._evict_last()
+            new_node = self.Node(key, value)
+            self._add_to_front(new_node)
+            self.cache[key] = new_node
 
     def _move_to_front(self, node):
         self._remove_node(node)
         self._add_to_front(node)
-    
+
     def _remove_node(self, node):
         node.prev.next = node.next
         node.next.prev = node.prev
@@ -56,18 +54,3 @@ class LRUCache:
         del self.cache[last_node.key]
         self._remove_node(last_node)
 
-
-cache = LRUCache(2)
-
-cache.set("k1", "val1")
-cache.set("k2", "val2")
-
-assert cache.get("k3") is None
-assert cache.get("k2") == "val2"
-assert cache.get("k1") == "val1"
-
-cache.set("k3", "val3")
-
-assert cache.get("k3") == "val3"
-assert cache.get("k2") is None
-assert cache.get("k1") == "val1"
