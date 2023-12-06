7e1045ee5bf6955ef862df5f742c58e0453a9540
Darya2477 <85761375+Darya2477@users.noreply.github.com>
2023-12-06 21:10:18 +0300

    Update test_lru.py

--- 05/test_lru.py
+++ 05/test_lru.py
@@ -1,35 +1,35 @@
 import unittest
 from lru import LRUCache
 
+
 class LRUTest(unittest.TestCase):
     def SetUp(self):
         pass
+
     def tearDown(self):
         pass
-    
+
     def test_limit_is_negative(self):
-        cache = LRUCache(limit=-1)
         with self.assertRaises(Exception):
-            cache.set("key1", 1)
+            LRUCache(limit=-1)
 
     def test_limit_is_zero(self):
-        cache = LRUCache(limit=0)
         with self.assertRaises(Exception):
-            cache.set("key1", 1)
-    
+            LRUCache(limit=0)
+
     def test_limit_is_one(self):
         cache = LRUCache(limit=1)
         cache.set("key1", 1)
         cache.set("key2", 2)
 
-        self.assertIsNone(cache.get("key1"))  
-        self.assertEqual(cache.get("key2"), 2) 
+        self.assertIsNone(cache.get("key1"))
+        self.assertEqual(cache.get("key2"), 2)
 
     def test_empty_cache(self):
         cache = LRUCache(limit=3)
 
-        self.assertIsNone(cache.get("key1"))  
-        self.assertIsNone(cache.get("key2")) 
+        self.assertIsNone(cache.get("key1"))
+        self.assertIsNone(cache.get("key2"))
 
     def test_cache_update_existing_key(self):
         cache = LRUCache(limit=3)
@@ -38,8 +38,8 @@ class LRUTest(unittest.TestCase):
 
         cache.set("key2", 20)
 
-        self.assertEqual(cache.get("key1"), 1)  
-        self.assertEqual(cache.get("key2"), 20) 
+        self.assertEqual(cache.get("key1"), 1)
+        self.assertEqual(cache.get("key2"), 20)
 
     def test_cache_set_and_get(self):
         cache = LRUCache(limit=3)
@@ -47,10 +47,10 @@ class LRUTest(unittest.TestCase):
         cache.set("key2", 2)
         cache.set("key3", 3)
 
-        self.assertEqual(cache.get("key1"), 1)  
-        self.assertEqual(cache.get("key2"), 2)  
-        self.assertEqual(cache.get("key3"), 3)  
-        self.assertIsNone(cache.get("key4"))    
+        self.assertEqual(cache.get("key1"), 1)
+        self.assertEqual(cache.get("key2"), 2)
+        self.assertEqual(cache.get("key3"), 3)
+        self.assertIsNone(cache.get("key4"))
 
     def test_cache_overflow(self):
         cache = LRUCache(limit=2)
@@ -60,8 +60,8 @@ class LRUTest(unittest.TestCase):
         cache.set("key3", 3)
         cache.set("key4", 4)
 
-        self.assertIsNone(cache.get("key1")) 
-        self.assertIsNone(cache.get("key2")) 
+        self.assertIsNone(cache.get("key1"))
+        self.assertIsNone(cache.get("key2"))
         self.assertEqual(cache.get("key3"), 3)
         self.assertEqual(cache.get("key4"), 4)
 
@@ -74,7 +74,29 @@ class LRUTest(unittest.TestCase):
         cache.get("key1")
         cache.set("key4", 4)
 
-        self.assertEqual(cache.get("key1"), 1)  
-        self.assertIsNone(cache.get("key2"))   
-        self.assertEqual(cache.get("key3"), 3)  
-        self.assertEqual(cache.get("key4"), 4)  
+        self.assertEqual(cache.get("key1"), 1)
+        self.assertIsNone(cache.get("key2"))
+        self.assertEqual(cache.get("key3"), 3)
+        self.assertEqual(cache.get("key4"), 4)
+
+    def test_cache_with_capacity_1(self):
+        cache = LRUCache(1)
+        cache.set("k1", "val1")
+        cache.set("k2", "val2")
+        self.assertEqual(cache.get("k1"), None)
+        self.assertEqual(cache.get("k2"), "val2")
+
+    def test_change_value_existing_key(self):
+        cache = LRUCache(2)
+        cache.set("k1", "val1")
+        cache.set("k2", "val2")
+        cache.set("k1", "val1_new")
+        cache.set("k3", "val3")
+
+        self.assertEqual(cache.get("k1"), "val1_new")
+        self.assertEqual(cache.get("k3"), "val3")
+        self.assertEqual(cache.get("k2"), None)
+
+
+if __name__ == "__main__":
+    unittest.main()
